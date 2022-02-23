679932f647e2931d530a0c1e5723b91c9f1f6178
Diogo Correia <dcorreia@wavecom.pt>
2022-02-23 19:29:05 +0000

    fixed linting errors + server config object is now context of the server app __init__ file

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,8 +6,9 @@ from seizmeia.server.auth.config import Config, EncryptionAlgorithm
 
 
 def test_config_from_yaml():
-    doc = """
-    secretKey: 09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7
+    secret = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    doc = f"""
+    secretKey: {secret}
     encrytionAlgorithm: HS256
     tokenExpirationTime: P7DT0H0M0S # ISO 8601
     """
@@ -17,7 +18,7 @@ def test_config_from_yaml():
     conf = Config(**yaml.safe_load(reader))
 
     assert conf == Config(
-        secretKey="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7",
+        secretKey=secret,
         encrytionAlgorithm=EncryptionAlgorithm.HS256,
         tokenExpirationTime=timedelta(days=7),
     )
